13dc789f89e811af4e6ed60a7ccb9b75230dfda4
Настя Жукова <zhukova.nastya2018@yandex.ru>
2024-03-12 20:12:14 +0300

    Добавьте файлы проекта.

---
+++ test/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WorkingDateTime;
+using LeapYear;
+using TimeDifference;
+using FindingDay;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WorkingDateTime.Form1 form = new WorkingDateTime.Form1();
+            form.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LeapYear.Form1 form = new LeapYear.Form1();
+            form.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TimeDifference.Form1 form = new TimeDifference.Form1();
+            form.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FindingDay.Form1 form = new FindingDay.Form1();
+            form.Show();
+        }
+    }
+}
